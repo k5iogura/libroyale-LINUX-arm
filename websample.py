0d78323b0c3adc29a38493f3634424cdf8723691
Kenji Ogura <k5i.ogura.v40@gmail.com>
2018-11-06 10:05:36 +0000

    revise

--- websample.py
+++ websample.py
@@ -8,18 +8,20 @@ dev = openni2.Device.open_any()
 print(dev.get_sensor_info(openni2.SENSOR_DEPTH))
 
 depth_stream = dev.create_depth_stream()
+#depth_stream = dev.create_ir_stream()
 depth_stream.start()
-frame = depth_stream.read_frame()
-frame_data = frame.get_buffer_as_uint16()
-depth_stream.stop()
-
-depth_array = np.ndarray((frame.height,frame.width),dtype=np.uint16,buffer=frame_data)
-da1d = depth_array.reshape(frame.height*frame.width)
-mx1d = max(da1d)
-depth_array = depth_array / mx1d
-print(depth_array.shape)
-cv2.imshow('depth_array', depth_array)
+#print(depth_array.shape)
 while True:
+    frame = depth_stream.read_frame()
+    frame_data = frame.get_buffer_as_uint16()
+    depth_array = np.ndarray((frame.height,frame.width),dtype=np.uint16,buffer=frame_data)
+    da1d = depth_array.reshape(frame.height*frame.width)
+    mx1d = max(da1d)
+    depth_array = depth_array / mx1d
+    depth_array = convertTo(depth_array, cv2.CV_8UC1)
+    depth_array = cv2.applyColorMap(depth_array, cv2.COLORMAP_JET)
+    cv2.imshow('depth_array', depth_array)
     if cv2.waitKey(1) != -1: break
 
+depth_stream.stop()
 openni2.unload()
