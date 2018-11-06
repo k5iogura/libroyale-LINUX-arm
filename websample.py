b7ff8de06b61e1e949ae38ff56797a47d406d8a8
Kenji Ogura <k5i.ogura.v40@gmail.com>
2018-11-06 23:14:56 +0000

    revise

--- websample.py
+++ websample.py
@@ -1,6 +1,7 @@
 import argparse
 import cv2
 import numpy as np
+from pdb import *
 
 from primesense import openni2
 
@@ -14,18 +15,24 @@ dev = openni2.Device.open_any()
 print(dev.get_sensor_info(openni2.SENSOR_DEPTH))
 
 depth_stream = dev.create_depth_stream()
-#depth_stream = dev.create_ir_stream()
+#depth_stream = dev.create_ir_stream() # flexx not support
 depth_stream.start()
-#print(depth_array.shape)
+
+dmax=mean=0
 while True:
     frame = depth_stream.read_frame()
     frame_data = frame.get_buffer_as_uint16()
     depth_array = np.ndarray((frame.height,frame.width),dtype=np.uint16,buffer=frame_data)
-    da1d = depth_array.reshape(frame.height*frame.width)
-    mx1d = max(da1d)
-    depth_array = depth_array / mx1d
+    mean = np.mean(depth_array)
+    depth_array = depth_array / (2*mean)
+    depth_array[depth_array>1.0] = 1.0
+    depth_array *= 255
+    depth_array = depth_array.astype(np.uint8)
+    depth_array = cv2.applyColorMap(depth_array, cv2.COLORMAP_PINK)
     cv2.imshow('depth_array', depth_array)
     if cv2.waitKey(args.waitKey) != -1: break
+print("data max ",dmax)
+print("data mean",mean)
 
 depth_stream.stop()
 openni2.unload()
